27842db43d45af5e0cd6b02c45a89afad39198a9
胡鑫 <269476228@qq.com>
2019-03-08 18:04:40 +0800

    修复百度统计

--- back-end/PHP/laravel/resources/views/home/layouts/home.blade.php
+++ back-end/PHP/laravel/resources/views/home/layouts/home.blade.php
@@ -40,7 +40,7 @@
     <link rel="stylesheet" href="{{asset('css/home/index.css')}}">
     <link rel="stylesheet" href="{{asset('css/sweetalert2.min.css')}}">
     @stack('other-css')
-    {!! config('websiteConfig.web_count') !!}
+    {!! config('websiteConfig.baidu_tongji') !!}
 
 </head>
 <body>
@@ -354,7 +354,7 @@
     });
     let APP_URL = "{{config('app.url')}}"
 </script>
-{!! config('websiteConfig.web_push') !!}
+{!! config('websiteConfig.baidu_tuisong') !!}
 <script src="{{asset('js/layer/layer.js')}}"></script>
 <script src="{{asset('js/sweetalert2.all.min.js')}}"></script>
 <script src="{{asset('js/common.js'.'?ver='.RELEASE_VERSION)}}"></script>
