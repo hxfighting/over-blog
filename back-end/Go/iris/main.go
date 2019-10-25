70b668c77fcbad7dc97817456fdba67ee6143d39
胡鑫 <269476228@qq.com>
2019-10-25 13:01:19 +0800

    优化golang的panic异常捕获，记录到错误日志

--- back-end/Go/iris/main.go
+++ back-end/Go/iris/main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"blog/queue"
 	"blog/routes"
+	"blog/service"
 	stdContext "context"
+	"fmt"
 	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/recover"
+	"github.com/kataras/iris/context"
 	"log"
 	"os"
 	"os/signal"
+	"runtime"
+	"strconv"
 	"syscall"
 	"time"
 )
 
 func main() {
 	app := iris.New()
-	app.Use(recover.New())
+	app.Use(panicCapture())
 	app.Use(cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
 		AllowCredentials: true,
@@ -30,6 +34,7 @@ func main() {
 	app.HandleDir("/js", "./public/js")
 	app.HandleDir("/image", "./public/image")
 	app.HandleDir("/static", "./public/static")
+
 	//app.RegisterView(iris.HTML("./views", ".html"))
 	routes.RegisterRoutes(app)
 	go func() {
@@ -59,4 +64,51 @@ func main() {
 	if run := app.Run(iris.Addr(":8080"), iris.WithoutInterruptHandler, iris.WithOptimizations); run != nil {
 		log.Fatalln(run.Error())
 	}
+}
+
+/**
+捕获博客的panic异常
+*/
+func panicCapture() context.Handler {
+	return func(ctx context.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				if ctx.IsStopped() {
+					return
+				}
+				var stacktrace string
+				for i := 1; ; i++ {
+					_, f, l, got := runtime.Caller(i)
+					if !got {
+						break
+					}
+
+					stacktrace += fmt.Sprintf("%s:%d\n", f, l)
+				}
+				// when stack finishes
+				logMessage := fmt.Sprintf("Recovered from a route's Handler('%s')\n", ctx.HandlerName())
+				logMessage += fmt.Sprintf("At Request: %s\n", getRequestLogs(ctx))
+				logMessage += fmt.Sprintf("Trace: %s\n", err)
+				logMessage += fmt.Sprintf("\n%s", stacktrace)
+				service.Log.Error(logMessage)
+				ctx.StatusCode(500)
+				ctx.StopExecution()
+			}
+		}()
+
+		ctx.Next()
+	}
+}
+
+/**
+获取请求数据
+*/
+func getRequestLogs(ctx context.Context) string {
+	var status, ip, method, path string
+	status = strconv.Itoa(ctx.GetStatusCode())
+	path = ctx.Path()
+	method = ctx.Method()
+	ip = ctx.RemoteAddr()
+	// the date should be logged by iris' Logger, so we skip them
+	return fmt.Sprintf("%v %s %s %s", status, path, method, ip)
 }
