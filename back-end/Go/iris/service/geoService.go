e03dc7755381d4b0095041ce4711ac1c292bbef6
胡鑫 <269476228@qq.com>
2019-10-26 14:14:18 +0800

    修改golang bug

--- back-end/Go/iris/service/geoService.go
+++ back-end/Go/iris/service/geoService.go
@@ -1,13 +1,17 @@
 package service
 
-import "github.com/oschwald/geoip2-golang"
+import (
+	"blog/config"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+)
 
 var GeoDB *geoip2.Reader
 
 func NewGeoDb() {
-	db, err := geoip2.Open("./geo.mmdb")
+	db, err := geoip2.Open(config.ConfigPath + "/geo.mmdb")
 	if err != nil {
-		Log.Error(err.Error())
+		log.Fatalln(err.Error())
 	}
 	GeoDB = db
 }
